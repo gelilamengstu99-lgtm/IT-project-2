21ba56eeb15914a3c29298a9bc4e6ed1966b941f
Gelila Mengstu <gelilamengstu99@gmail.com>
2025-11-03 12:06:39 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,33 @@
+x= int(input("you age please: "))
+
+if 0<x<12:
+    print("child")
+elif 12<x<17:
+	print("teenager")
+elif 17<x<64:
+	print("Adult")
+    
+else:
+    print("Senior")
+y= int(input("what time is it now: "))
+
+if 5<=y<=11:
+	print("morning")    
+elif 12<=y<=16:
+	print("afternoon")
+elif 17<=y<=20:
+	print("evening")
+else:print("night")
+
+z= int(input("enter a number:"))
+
+if z%5==0:
+	print(z,"is divisible by 5")
+else:print(z,"is not divisible by 5")
+    
+    
+    
+import http.client
+
+conn = http.client.HTTPSC
+
